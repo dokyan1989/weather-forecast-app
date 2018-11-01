7646ea88373ef50f829d9b4547350230abbab4c3
dokyan1989 <38043228+dokyan1989@users.noreply.github.com>
2018-11-01 15:01:37 +0700

    first commit

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = '483a0501896b3d602ecae43d2d56f060';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
